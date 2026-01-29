55f164c5d120e7ceee6b41f89dc7792ffffdfbb4
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-01-29 10:44:01 +0700

    Add Task model

---
+++ ToDoApp/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToDoApp.Extention;
+
+namespace ToDoApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
