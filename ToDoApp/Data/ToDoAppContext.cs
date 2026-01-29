7ec04d06a8f13d5378a05b13db99be4e9a9c55ae
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-01-29 14:18:33 +0700

    Scaffold task pages

---
+++ ToDoApp/Data/ToDoAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ToDoAppContext : DbContext
+    {
+        public ToDoAppContext (DbContextOptions<ToDoAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoApp.Models.Task> Task { get; set; } = default!;
+    }
+}
--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,35 +1,41 @@
-public partial class Program
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoApp.Data;
+namespace ToDoApp
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ToDoAppContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoAppContext") ?? throw new InvalidOperationException("Connection string 'ToDoAppContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Tasks}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapStaticAssets();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}")
-            .WithStaticAssets();
-
-
-        app.Run();
     }
 }
