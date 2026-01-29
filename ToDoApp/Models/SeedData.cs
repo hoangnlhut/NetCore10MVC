299643f3b89cbe1a71077e4b92778934a8d6bb1c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-01-29 14:36:35 +0700

    Seed the database

---
+++ ToDoApp/Models/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data;
+
+namespace ToDoApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ToDoAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ToDoAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.Task.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Task.AddRange(
+                    new Task
+                    {
+                        Title = "To Do App",
+                        ReleaseDate = DateTime.Parse("2026-1-29"),
+                        Description = "Create a to do app using Blazor and EF Core",
+                        Status = Extention.StatusEnum.InProgress
+                    },
+                    new Task
+                    {
+                        Title = "Complete fundamental .net course",
+                        Description = "Complete the .net fundamental course on Pluralsight",
+                        ReleaseDate = DateTime.Parse("2026-1-29"),
+                        Status = Extention.StatusEnum.InProgress
+                    },
+                    new Task
+                    {
+                        Title = "Complete advanced .net course",
+                        Description = "Complete the .net advanced course on Pluralsight",
+                        ReleaseDate = DateTime.Parse("2026-1-29"),
+                        Status = Extention.StatusEnum.InProgress
+                    },
+                    new Task
+                    {
+                        Title = "Complete microservice in .net course",
+                        Description = "Complete the microservice in .net course on Pluralsight",
+                        ReleaseDate = DateTime.Parse("2026-1-29"),
+                        Status = Extention.StatusEnum.InProgress
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoApp.Data;
+using ToDoApp.Models;
 namespace ToDoApp
 {
     public class Program
@@ -15,6 +16,13 @@ namespace ToDoApp
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
