fbd20d3a8737102d3f166832b63c799a6848dfb2
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-02-04 10:47:54 +0700

    Add Extention method for get infomation from HttpContext

--- HttpContextDemo/Controllers/HomeController.cs
+++ HttpContextDemo/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 using HttpContextDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using HttpContextDemo.Extentions;
 
 namespace HttpContextDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Handling Index request. TraceIdentifier: {TraceId} - request:  {ri}", HttpContext.TraceIdentifier, HttpContext.GetDebugInfo());
             return View();
         }
 
---
+++ HttpContextDemo/Extentions/HttpContextExtention.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpContextDemo.Extentions
+{
+    public static class HttpContextExtention
+    {
+        public static string GetDebugInfo(this HttpContext httpContext)
+        {
+            return $"{httpContext.Request.Scheme}://{httpContext.Request.Host}";
+        }
+    }
+}
