c7fbfc29270d7ca6f7ab0442e8fd2952740dade0
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-02-05 15:22:27 +0700

    HttpContext.Response: add some code to check some common function using in Response

---
+++ HttpContextDemo/Controllers/ResponseDemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContextDemo.Controllers
+{
+    public class ResponseDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test1() 
+        { 
+            return Content("This is a plain text response from Test1 action.");
+        }
+
+        public IActionResult Test2()
+        {
+            return Ok("Test2");
+        }
+
+        public IActionResult Bad()
+        {
+            return BadRequest();
+        }
+    }
+}
--- ToDoApp/Controllers/TasksController.cs
+++ ToDoApp/Controllers/TasksController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using ToDoApp.Data;
 using ToDoApp.Extention;
