44695ea8ce2a32615ac95af13fe33cc1393ac284
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-02-11 10:52:51 +0700

    Initiailize new project for configuration demo

---
+++ ConfigurationDemo/Program.cs
@@ -0,0 +1,35 @@
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var host = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!host.Environment.IsDevelopment())
+        {
+            host.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            host.UseHsts();
+        }
+
+        host.UseHttpsRedirection();
+        host.UseRouting();
+
+        host.UseAuthorization();
+
+        host.MapStaticAssets();
+
+        host.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+
+        host.Run();
+    }
+}
