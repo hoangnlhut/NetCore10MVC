f1c35113f8963d769bcaa23188ab52dac0513d87
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-02-24 10:30:52 +0700

    Session_ASPNETCORE_Demo: Initialize new project

---
+++ SessionAspCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SessionAspCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
