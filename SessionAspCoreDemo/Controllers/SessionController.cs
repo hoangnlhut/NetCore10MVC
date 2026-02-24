5cbe2ad203a58c465388297278c9030210d23673
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-02-24 11:43:31 +0700

    Session_ASPNETCORE_Demo: add session to register in DI in a way of only one instance in test or development environment

--- SessionAspCoreDemo/Controllers/SessionController.cs
+++ SessionAspCoreDemo/Controllers/SessionController.cs
@@ -8,5 +8,17 @@ namespace SessionAspCoreDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult SetSession(string key, string value)
+        {
+            HttpContext.Session.SetString(key, value);
+            return Ok($"Session Saved {key} and {value}");
+        }
+        public IActionResult GetSession(string key)
+        {
+            string? value = HttpContext.Session.GetString(key);
+            value = string.IsNullOrEmpty(value) ? "Unknown Key" : value;
+            return Ok($"Value of Session: {value}");
+        }
     }
 }
--- SessionAspCoreDemo/Program.cs
+++ SessionAspCoreDemo/Program.cs
@@ -2,8 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".SessionDemo";
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();
 
 app.MapStaticAssets();
 
